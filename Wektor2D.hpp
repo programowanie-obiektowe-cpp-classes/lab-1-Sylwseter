8a8a2962c6c0042ffdd1809a8c19a26fca18e35b
Michał Trawczyński <01171684@pw.edu.pl>
2024-10-28 11:50:11 +0100

    Dzień dobry, przesyłam zadnaie 1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,32 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x;
+    double y;
+
+public:
+    // Konstruktor domyœlny (wektor o wspó³rzêdnych [0, 0])
+    Wektor2D() : x(0), y(0) {}
+
+    // Konstruktor przyjmuj¹cy wspó³rzêdne
+    Wektor2D(double x, double y) : x(x), y(y) {}
+
+    // Settery
+    void setX(double x) { this->x = x; }
+    void setY(double y) { this->y = y; }
+
+    // Gettery
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    // Operator dodawania wektorów
+    Wektor2D operator+(const Wektor2D& other) const {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+
+    // Operator mno¿enia skalarnego
+    double operator*(const Wektor2D& other) const {
+        return x * other.x + y * other.y;
+    }
 };
+
